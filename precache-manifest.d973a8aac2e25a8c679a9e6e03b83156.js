e9cfe155e1d01830b2d065288b8a47117e91db6e
bielJS <gabriel.ferreira@email.com>
2020-07-06 18:52:44 -0300

    Updates

---
+++ precache-manifest.d973a8aac2e25a8c679a9e6e03b83156.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e7babe9f68d732f2ff1e81e13023df",
+    "url": "/gitsearch/index.html"
+  },
+  {
+    "revision": "f9fcaa43c3f1e77d7696",
+    "url": "/gitsearch/static/css/main.65e7a17c.chunk.css"
+  },
+  {
+    "revision": "5dc2cacdb4cd472b63b5",
+    "url": "/gitsearch/static/js/2.f1ad1b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitsearch/static/js/2.f1ad1b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fcaa43c3f1e77d7696",
+    "url": "/gitsearch/static/js/main.f9ca195b.chunk.js"
+  },
+  {
+    "revision": "b5dddb3901b8aae4fab9",
+    "url": "/gitsearch/static/js/runtime-main.c24412f1.js"
+  },
+  {
+    "revision": "e06e4c142785b67cee7079c7a25c19aa",
+    "url": "/gitsearch/static/media/Github.e06e4c14.png"
+  },
+  {
+    "revision": "29d553f34941fdb54483e9cf9078db79",
+    "url": "/gitsearch/static/media/Search.29d553f3.svg"
+  }
+]);
